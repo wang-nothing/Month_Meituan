2bdfcc3a91693a9c2533c4990525ba2ef27bd0dd
wang-nothing <773516330@qq.com>
2018-07-20 15:18:53 +0800

    第三方登录添加

--- app/src/main/java/com/example/admin/month_meituan/my/login/view/LoginActivity.java
+++ app/src/main/java/com/example/admin/month_meituan/my/login/view/LoginActivity.java
@@ -7,32 +7,109 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.example.admin.month_meituan.R;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements Ilogin_Iview, View.OnClickListener {
+import com.example.admin.month_meituan.R;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
     private EditText login_mobile, login_password;
     private Button login_btn_login;
     private ProgressDialog dialog;;
-
+    private UMShareAPI mShareAPI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mShareAPI = UMShareAPI.get(this);
 
     }
 
     @Override
-    public void onClick(View view) {
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
     }
 
-    @Override
-    public void view_onSuccess(String mobile, String password) {
+    public void login(View view) {
+        mShareAPI.getPlatformInfo(LoginActivity.this, SHARE_MEDIA.QQ, authListener);
+        UMImage image=new UMImage(LoginActivity.this,R.mipmap.ic_launcher);
+        new ShareAction(LoginActivity.this).withText("hell")
+                .withMedia(image)
+                .setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ, SHARE_MEDIA.WEIXIN)
+                .setCallback(new UMShareListener() {
+                    @Override
+                    public void onStart(SHARE_MEDIA share_media) {
+
+                    }
+
+                    @Override
+                    public void onResult(SHARE_MEDIA share_media) {
+
+                    }
+
+                    @Override
+                    public void onError(SHARE_MEDIA share_media, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCancel(SHARE_MEDIA share_media) {
+
+                    }
+                }).open();
 
     }
+    UMAuthListener authListener = new UMAuthListener() {
+        /**
+         * @desc 授权开始的回调
+         * @param platform 平台名称
+         */
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
 
-    @Override
-    public void view_onFail(int code) {
+        }
 
-    }
+        /**
+         * @desc 授权成功的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         * @param data 用户资料返回
+         */
+        @Override
+        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+            Map<String,String>data1=data;
+            Toast.makeText(LoginActivity.this, "成功了", Toast.LENGTH_LONG).show();
+
+        }
+
+        /**
+         * @desc 授权失败的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         * @param t 错误原因
+         */
+        @Override
+        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+
+            Toast.makeText(LoginActivity.this, "失败：" + t.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+        /**
+         * @desc 授权取消的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上5
+         */
+        @Override
+        public void onCancel(SHARE_MEDIA platform, int action) {
+            Toast.makeText(LoginActivity.this, "取消了", Toast.LENGTH_LONG).show();
+        }
+    };
 }
---
+++ app/src/main/java/com/example/admin/month_meituan/wxapi/WXEntryActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.month_meituan.wxapi;
+
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by 1 on 2018/7/18.
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+}
